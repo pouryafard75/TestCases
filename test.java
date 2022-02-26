2c7ab98ce2f73a0dc13fe5bc2db52d87688b93dd
pouryafard75 <pouryafard75@gmail.com>
2022-02-26 02:23:02 -0500

    order changed

--- test.java
+++ test.java
@@ -1,12 +1,13 @@
 public class test {
-    public static void main(String[] args) {
-        System.out.println(10/2);
+    public static void printAvg(int sum, int count){
+        System.out.println(sum / count);
     }
+
     public static void Test()
     {
         System.out.println("Test");
     }
-    public static void printAvg(int sum, int count) {
-        //Todo:
+    public static void main(String[] args) {
+        printAvg(10,2);
     }
 }
