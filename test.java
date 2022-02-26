6b2fddb9bef48bc6929f6b304b157d0990bc9770
pouryafard75 <pouryafard75@gmail.com>
2022-02-25 22:00:14 -0500

    v1

---
+++ test.java
@@ -0,0 +1,8 @@
+public class v1 {
+    public static void main(String[] args) {
+        System.out.println(10/2);
+    }
+    public static void printAvg(int sum, int count) {
+        //Todo:
+    }
+}
