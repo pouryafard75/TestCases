2b7bd4375d953799a87ad6ed21f014679c1b5310
pouryafard75 <pouryafard75@gmail.com>
2022-02-26 02:21:23 -0500

    New function added

--- test.java
+++ test.java
@@ -1,9 +1,12 @@
 public class test {
-    public static void printAvg(int sum, int count){
-        System.out.println(sum / count);
-    }
     public static void main(String[] args) {
-        printAvg(10,2);
+        System.out.println(10/2);
+    }
+    public static void Test()
+    {
+        System.out.println("Test");
+    }
+    public static void printAvg(int sum, int count) {
+        //Todo:
     }
 }
-
