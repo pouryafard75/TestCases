7d9d56ad3014983cd177e98f8b51e667e0cbffe6
pouryafard75 <pouryafard75@gmail.com>
2022-02-28 18:51:08 -0500

    v2

--- printAvg/printAvgTest.java
+++ printAvg/printAvgTest.java
@@ -1,9 +1,9 @@
 package printAvg;
 public class printAvgTest {
     public static void main(String[] args) {
-        System.out.println(10/2);
+        printAvg(10,2);
     }
-    public static void printAvg(int sum, int count) {
-        //Todo:
+    public static void printAvg(int sum, int count){
+        System.out.println(sum / count);
     }
 }
