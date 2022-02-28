7c1fa47dde90d3c53567b26f3374edd2c092009d
pouryafard75 <pouryafard75@gmail.com>
2022-02-28 18:40:23 -0500

    Order Change for printAvg

--- printAvg/printAvg.java
+++ printAvg/printAvg.java
@@ -1,11 +1,7 @@
-public class test {
+package printAvg;
+public class printAvg {
     public static void printAvg(int sum, int count){
         System.out.println(sum / count);
-    }
-
-    public static void Test()
-    {
-        System.out.println("Test");
     }
     public static void main(String[] args) {
         printAvg(10,2);
---
+++ printAvg/v2.java
@@ -0,0 +1,9 @@
+package printAvg;
+public class v2 {
+    public static void main(String[] args) {
+        printAvg(10,2);
+    }
+    public static void printAvg(int sum, int count){
+        System.out.println(sum / count);
+    }
+}
