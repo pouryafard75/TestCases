5d0fd1ea6997d7a920ec424c5019af5d5ff36c4b
pouryafard75 <pouryafard75@gmail.com>
2022-02-28 19:56:12 -0500

    addSharpTest v1

--- addSharp/addSharpTest.java
+++ addSharp/addSharpTest.java
@@ -2,18 +2,15 @@ package addSharp;
 public class addSharpTest{
     public static void f2()
     {
-        System.out.println(addSharp("f2"));
+        System.out.println("f2" + "#");
     }
     public static void f1()
     {
-        System.out.println(addSharp("f1"));
+        System.out.println("f1" + "#");
     }
 
     public static void main(String[] args) {
         f1();
         f2();
-    }
-    public static String addSharp(String inp){
-        return inp + "#";
     }
 }
