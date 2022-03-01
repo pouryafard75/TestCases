8a091dc36d0e86c8caa7af989d66840a6234f569
pouryafard75 <pouryafard75@gmail.com>
2022-02-28 19:33:48 -0500

    printAvgTest order change v2

--- printAvg/v2.java
+++ printAvg/v2.java
@@ -3,10 +3,6 @@ public class printAvgTest {
     public static void printAvg(int sum, int count){
         System.out.println(sum / count);
     }
-    public static void Test()
-    {
-        System.out.println("Test");
-    }
     public static void main(String[] args) {
         printAvg(10,2);
     }
