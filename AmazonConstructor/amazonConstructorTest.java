fb0aff67073171c2e2bb3ba542436c3c31babfd0
pouryafard75 <pouryafard75@gmail.com>
2022-02-28 23:04:49 -0500

    amazonConstructor v2

--- AmazonConstructor/amazonConstructorTest.java
+++ AmazonConstructor/amazonConstructorTest.java
@@ -2,14 +2,22 @@ package amazonConstructor;
 public class AmazonElasticFileSystemAsyncClient extends
         AmazonElasticFileSystemClient implements AmazonElasticFileSystemAsync {
 
-    public AmazonElasticFileSystemAsyncClient(AWSCredentialsProvider awsCredentialsProvider,
-            ExecutorService executorService) {
-        this(awsCredentialsProvider, new ClientConfiguration(), executorService);
+    public AmazonElasticFileSystemAsyncClient(
+            com.amazonaws.auth.AWSCredentialsProvider awsCredentialsProvider,
+            com.amazonaws.ClientConfiguration clientConfiguration) {
+
+        this(awsCredentialsProvider, clientConfiguration,
+                java.util.concurrent.Executors
+                        .newFixedThreadPool(clientConfiguration
+                                .getMaxConnections()));
     }
 
-    public AmazonElasticFileSystemAsyncClient(AWSCredentialsProvider awsCredentialsProvider,
-            ClientConfiguration clientConfiguration) {
-        this(awsCredentialsProvider, clientConfiguration,
-                Executors.newFixedThreadPool(clientConfiguration.getMaxConnections()));
+    public AmazonElasticFileSystemAsyncClient(
+            com.amazonaws.auth.AWSCredentialsProvider awsCredentialsProvider,
+            java.util.concurrent.ExecutorService executorService) {
+
+        this(awsCredentialsProvider, new com.amazonaws.ClientConfiguration(),
+                executorService);
     }
+
 }
