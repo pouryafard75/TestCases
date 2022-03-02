fb9a8dac91a37cec61bef8171c1e9c77d859737d
pouryafard75 <pouryafard75@gmail.com>
2022-03-02 18:20:14 -0500

    pureReodering

---
+++ pureReorder/v2.java
@@ -0,0 +1,27 @@
+public class test {
+    public static int getSum()
+    {
+        //1
+        int sum = 0;
+        for (int i = 0; i < 10; i++) 
+            sum += i;
+        return sum;
+    }
+    public static float getDividedSum(float f)
+    {
+        //3
+        int sum = 0;
+        for (int i = 0; i < 10; i++) 
+            sum += i;
+        return (float)sum / f;
+    }
+    public static void printSum()
+    {
+        //2
+        int sum = 0;
+        for (int i = 0; i < 10; i++) 
+            sum += i;
+        System.out.println(sum);
+    }
+}
+//132
