1e0b1d14676cc840d30bc964581608b18d01354c
pouryafard75 <pouryafard75@gmail.com>
2022-03-21 14:12:39 -0400

    debug

--- 3March/addSharp/v2.java
+++ 3March/addSharp/v2.java
@@ -1,14 +1,13 @@
 package addSharp;
 public class addSharpTest{
-    public static void f2()
-    {
-        System.out.println(addSharp("f2"));
-    }
     public static void f1()
     {
         System.out.println(addSharp("f1"));
     }
-    
+    public static void f2()
+    {
+        System.out.println(addSharp("f2"));
+    }
 
     public static void main(String[] args) {
         f1();
