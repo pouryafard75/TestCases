875417b261a3349bafca984574dbdf36eea5116b
pouryafard75 <pouryafard75@gmail.com>
2022-03-22 13:15:45 -0400

    JS TestCases Added

--- OldCases/addSharp/v2.java
+++ OldCases/addSharp/v2.java
@@ -1,13 +1,16 @@
 package addSharp;
-public class addSharpTest{
+public class addSharpTest2{
     public static void f1()
     {
         System.out.println(addSharp("f1"));
     }
     public static void f2()
     {
+        new c
         System.out.println(addSharp("f2"));
     }
+
+
 
     public static void main(String[] args) {
         f1();
