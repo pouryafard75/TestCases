d6b71d4a3486531442e9687f63e718d1286dfa3e
pouryafard <pouryafard75@gmail.com>
2022-05-02 13:04:44 -0400

    Test RenameMethod

--- NewCases/RenameMethod/v2/test.java
+++ NewCases/RenameMethod/v2/test.java
@@ -1,17 +1,14 @@
-public class TestRenameMethod{
-    public void calculateWeeklyRate(int a, int b, int purity)
-    {
-        rate = doSomeCalculation(a,b);
-        pure_rate = pure_rate(rate,purity);
+public class TestRenameMethod {
+    public void getWeeklyRate(int a, int b, int purity) {
+        rate = doSomeCalculation(a, b);
+        pure_rate = pure_rate(rate, purity);
         weekly = pure_rate * 7;
         try {
             insertToDatabase(weekly);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Something went wrong.");
-          }
+        }
 
         return weekly;
     }
-}
 }
