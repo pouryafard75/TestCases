caa79326a3e32ac275341d23dc8d95b69677ff53
pouryafard75 <pouryafard75@gmail.com>
2022-07-16 23:14:19 -0400

    V2 LambdaExample

--- Requested/Lambda/lambdaExample.java
+++ Requested/Lambda/lambdaExample.java
@@ -1,39 +1,23 @@
 public class lambdaExample{
     public int read() throws IOException {
-        if (!getInitialized()) {
-            initialize();
-            setInitialized(true);
-        }
-        int ch = -1;
-        if (line != null) {
-            ch = line.charAt(0);
-            if (line.length() == 1) {
-                line = null;
-            } else {
-                line = line.substring(1);
-            }
-        } else {
-            final int containsSize = contains.size();
-            for (line = readLine();
-                 line != null;
-                 line = readLine()) {
-                boolean matches = true;
-                for (int i = 0; matches && i<containsSize; i++) {
-                    String containsStr =
-                            (String)contains.elementAt(i);
-                    matches = line.indexOf(containsStr)>=0;
+        return extracted(contains,
+                (Integer i) -> {
+                    String containsStr = (String)contains.elementAt(i);
+                    return line.indexOf(containsStr)>=0;
                 }
-                if (matches ^ isNegated()) {
-                    break;
-                }
-            }
-            if (line != null) {
-                return read();
-            }
-        }
-        return ch;
+        );
     }
-    public int read2()
+    public int read2() throws IOException {
+        return extracted(regexps,
+                (Integer i) -> {
+                    RegularExpression regexp = (RegularExpression)regexps.elementAt(i);
+                    Regexp re = regexp.getRegexp(getProject());
+                    return re.matches(line);
+                }
+        );
+    }
+    protected int extracted(Vector vector,
+                            Function<Integer, Boolean> matcher)
             throws IOException {
         if (!getInitialized()) {
             initialize();
@@ -48,16 +32,13 @@ public class lambdaExample{
                 line = line.substring(1);
             }
         } else {
-            final int regexpsSize = regexps.size();
+            final int containsSize = vector.size();
             for (line = readLine();
                  line != null;
                  line = readLine()) {
                 boolean matches = true;
-                for (int i = 0; matches && i<regexpsSize; i++) {
-                    RegularExpression regexp =
-                            (RegularExpression)regexps.elementAt(i);
-                    Regexp re = regexp.getRegexp(getProject());
-                    matches = re.matches(line);
+                for (int i = 0; matches && i<containsSize; i++) {
+                    matches = (boolean)matcher.apply(i);
                 }
                 if (matches ^ isNegated()) {
                     break;
