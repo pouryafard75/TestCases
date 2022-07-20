4ff8989bc285e0378e03f2ec4b58ab7e367c8c58
pouryafard <pouryafard75@gmail.com>
2022-07-20 15:52:54 -0400

    addSharpEdited

--- v1/addSharp.java
+++ v1/addSharp.java
@@ -8,7 +8,6 @@ public class addSharpTest {
     public static void f2() {
         System.out.println("f2" + "#");
     }
-    
 
     public static void main(String[] args) {
         f1();
--- v2/addSharp.java
+++ v2/addSharp.java
@@ -5,11 +5,9 @@ public class addSharpTest {
         System.out.println(addSharp("f1"));
     }
 
-    
     public static void f2() {
         System.out.println(addSharp("f2"));
     }
-
 
     public static void main(String[] args) {
         f2();
