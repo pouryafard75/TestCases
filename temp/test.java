dfcfe50d1eef3cd716d0a26d9f617dabba2584d3
pouryafard <pouryafard75@gmail.com>
2022-07-25 15:46:08 -0400

    V2

--- temp/test.java
+++ temp/test.java
@@ -1,6 +1,6 @@
 public class test {
     public void a(RefactoringType refactoringType, float precision, float recall) {
-        System.out.println("F-score for " + refactoringType.getDisplayName() + " refactoring is: "
-                + (2 * precision * recall) / (float) (precision + recall));
+        fscore = temp();
+        System.out.println("F-score for " + refactoringType.getDisplayName() + " refactoring is: " + fScore);
     }
 }
