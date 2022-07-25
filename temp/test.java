170ff27695c66f2492e11f3b455b368c2badf5cc
pouryafard <pouryafard75@gmail.com>
2022-07-25 16:51:11 -0400

    foreach v1

--- temp/test.java
+++ temp/test.java
@@ -1,6 +1,6 @@
 public class test {
     public void a(RefactoringType refactoringType, float precision, float recall) {
-        fscore = temp();
-        System.out.println("F-score for " + refactoringType.getDisplayName() + " refactoring is: " + fScore);
+        for (Replacement replacement : refactoring.getReplacements())
+            System.out.println("ola");
     }
 }
