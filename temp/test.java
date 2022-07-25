f6998debdc897519beb8467854a0dc754c7b2928
pouryafard <pouryafard75@gmail.com>
2022-07-25 17:05:18 -0400

    foreach v3

--- temp/test.java
+++ temp/test.java
@@ -1,6 +1,7 @@
 public class test {
     public void a(RefactoringType refactoringType, float precision, float recall) {
-        for (Replacement replacement : refactoring.getReplacements())
+        for (Replacement replacement : refactoring.getReplacements()) {
             System.out.println("olaaa");
+        }
     }
 }
