3eedc184815d0618239abe00bb88816d71da3313
pouryafard <pouryafard75@gmail.com>
2022-07-27 16:14:15 -0400

    switchV3

--- switch/temp.java
+++ switch/temp.java
@@ -5,18 +5,6 @@ public class temp {
       case 7:
         System.out.println("Sunday");
         break;
-      case 6:
-        System.out.println("Saturday");
-        break;
-      case 5:
-        System.out.println("Friday");
-        break;
-      case 4:
-        System.out.println("Thursday");
-        break;
-      case 3:
-        System.out.println("Wednesday");
-        break;
       case 2:
         System.out.println("Tuesday");
         break;
