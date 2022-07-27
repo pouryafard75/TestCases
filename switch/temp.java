6e0e84b0aecc8bd3998b6d156a4745ccf6576616
pouryafard <pouryafard75@gmail.com>
2022-07-27 16:10:15 -0400

    switch v2

--- switch/temp.java
+++ switch/temp.java
@@ -2,27 +2,28 @@ public class temp {
   public void name() {
     int day = 4;
     switch (day) {
-      case 1:
-        System.out.println("Monday");
-        break;
-      case 2:
-        System.out.println("Tuesday");
-        break;
-      case 3:
-        System.out.println("Wednesday");
-        break;
-      case 4:
-        System.out.println("Thursday");
-        break;
-      case 5:
-        System.out.println("Friday");
+      case 7:
+        System.out.println("Sunday");
         break;
       case 6:
         System.out.println("Saturday");
         break;
-      case 7:
-        System.out.println("Sunday");
+      case 5:
+        System.out.println("Friday");
         break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 1:
+        System.out.println("Monday");
+        break;
+
     }
 
   }
