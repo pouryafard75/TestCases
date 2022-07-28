a876f4db2efd29a63097972c0dbd6d77f885d774
pouryafard <pouryafard75@gmail.com>
2022-07-28 18:54:46 -0400

    V1

---
+++ classDiff/temp.java
@@ -0,0 +1,15 @@
+package classDiff;
+
+public class temp {
+    private class inner1 {
+        public void print() {
+            System.out.println("This is an inner class 1");
+        }
+    }
+
+    private class inner2 {
+        public void print() {
+            System.out.println("This is an inner class 2");
+        }
+    }
+}
