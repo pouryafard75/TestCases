794576dfbd289c2ac361882ff22de3ee4aa1a36c
pouryafard <pouryafard75@gmail.com>
2022-08-01 14:34:46 -0400

    newSimple Renamed

---
+++ testCommit/newSimple.java
@@ -0,0 +1,19 @@
+package v1;
+
+public class newSimple {
+    private void f1() {
+        NewMethod();
+        newStatemenet();
+        D();
+    }
+
+    void f2() {
+        C();
+        NewMethod();
+    }
+
+    private void NewMethod() {
+        A();
+        B();
+    }
+}
