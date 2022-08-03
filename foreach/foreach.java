b0c5eeca2e0cd3b3cf4f35da0f1e19cc2fcbbe72
pouryafard <pouryafard75@gmail.com>
2022-08-02 23:06:08 -0400

    v1

---
+++ foreach/foreach.java
@@ -0,0 +1,11 @@
+public class foreach {
+    public static void main(String[] args) {
+        for (AbstractCodeFragment leaf : composite.getAdditionallyMatchedStatements1()) {
+            mapping.inlinedVariableAssignment(leaf, leaves2, parentMapper != null);
+            if (mapping.isIdenticalWithInlinedVariable()) {
+                break;
+            }
+        }
+    }
+
+}
