2e4e2e02e983ee08d245b3124f061f95e181ddcd
pouryafard <pouryafard75@gmail.com>
2022-08-30 19:07:14 -0400

    v1

---
+++ sout/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        bQuick = System.nanoTime();
+        System.err.println(testable.getName() + "InsertionSort.");
+    }
+
+}
