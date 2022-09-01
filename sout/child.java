466d59cd74fcfe447163e4b499400e2b67fa2be7
pouryafard <pouryafard75@gmail.com>
2022-08-31 20:13:04 -0400

    TempFiles

---
+++ sout/child.java
@@ -0,0 +1,10 @@
+class test {
+    void main() {
+        // System.err.println("QuickSort failed.");
+        try {
+            unsorted = unsorteds[i];
+        } catch (Exception e) {
+            // Block of code to handle errors
+        }
+    }
+}
---
+++ sout/parent.java
@@ -0,0 +1,10 @@
+class test {
+    void main() {
+        // System.err.println(testable.getName() + "InsertionSort.");
+        try {
+            Integer[] unsorted = unsorteds[i];
+        } catch (Exception e) {
+            // Block of code to handle errors
+        }
+    }
+}
