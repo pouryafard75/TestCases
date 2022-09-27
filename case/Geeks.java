94a3a897d08a91235aa35f38b12a12bdaae38c8e
pouryafard <pouryafard75@gmail.com>
2022-09-27 17:29:47 -0400

    static import v2

--- case/Geeks.java
+++ case/Geeks.java
@@ -1,12 +1,14 @@
 import static java.lang.Integer.*;
 import static java.lang.Byte.*;
-import com.github.javaparser.GeneratedJavaParserConstants;
+import static com.github.javaparser.GeneratedJavaParserConstants.*;
 
 enum Direction {
     left, right, top, down
 }
 
 class Geeks {
+    int a = 1;
+
     void run() {
         System.out.println("Run");
     }
