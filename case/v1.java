88c9540409c0fa2dc01e9de61978444abb479979
pouryafard <pouryafard75@gmail.com>
2022-09-27 16:42:21 -0400

    v1

---
+++ case/v1.java
@@ -0,0 +1,21 @@
+import static java.lang.Integer.*;
+import static java.lang.Byte.*;
+import static com.github.javaparser.GeneratedJavaParserConstants.*;
+
+class Geeks {
+    void run() {
+        System.out.println("Run");
+    }
+
+    ITest test = new ITest() {
+        @Override
+        public void print() {
+            System.out.println("ola");
+        }
+
+        @Override
+        public void sleep() {
+            sleep(10);
+        }
+    };
+}
