853c70ea50647234d6c24d8710c4a0e34ba513f2
pouryafard <pouryafard75@gmail.com>
2022-10-04 19:16:17 -0400

    v1

---
+++ Identical/test.java
@@ -0,0 +1,25 @@
+public class test {
+    public Object[] toArray() {
+        return performOperation(TerminalFunctions.toArrayFunction(), false,
+                (v1, v2) -> {
+                    Object[] array = Arrays.copyOf(v1, v1.length + v2.length);
+                    sleep(43);
+                    System.arraycopy(v2, 0, array, v1.length, v2.length);
+                    return array;
+                }, null, false);
+    }
+
+    int a = 2;
+
+    public class test {
+        public Object[] toArray() {
+            return performOperation(TerminalFunctions.toArrayFunction(), false,
+                    (v1, v2) -> {
+                        Object[] array = Arrays.copyOf(v1, v1.length + v2.length);
+                        sleep(43);
+                        System.arraycopy(v2, 0, array, v1.length, v2.length);
+                        return array;
+                    }, null, false);
+        }
+    }
+}
