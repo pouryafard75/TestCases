9bfec59746de9bf6bc783aab5c68aa5ab8bbdd6d
Pouryafard75 <pouryafard75@gmail.com>
2022-10-26 22:59:11 -0400

    v2 qualifed

--- bug/test.java
+++ bug/test.java
@@ -1,8 +1,8 @@
 public class v1 {
 
     void main() {
-        Set<Integer> ourSegments = UMLOperationBodyMapper.SPLIT_CONCAT_STRING_PATTERN;
-        
+        Set<Integer> ourSegments = SPLIT_CONCAT_STRING_PATTERN;
+
     }
 }
 
