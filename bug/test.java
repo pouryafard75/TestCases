7ebabe3f186b086704804a1272093d4d013d3fc6
Pouryafard75 <pouryafard75@gmail.com>
2022-10-26 22:57:10 -0400

    v1 qualifed

--- bug/test.java
+++ bug/test.java
@@ -1,11 +1,9 @@
-public class v3 {
+public class v1 {
 
     void main() {
-        for (Filter filter : filters.getFilters()) {
-            if (filter instanceof ExternalResourceHolder) {
-                final Set<String> locations = ((ExternalResourceHolder) filter).getExternalResourceLocations();
-                externalResources.addAll(locations);
-            }
-        }
+        Set<Integer> ourSegments = UMLOperationBodyMapper.SPLIT_CONCAT_STRING_PATTERN;
+        
     }
 }
+
+
