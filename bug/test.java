ad1fec1e1a193125a72cc6efba30a454749656ec
Pouryafard75 <pouryafard75@gmail.com>
2022-12-26 23:17:40 -0500

    v2 test

--- bug/test.java
+++ bug/test.java
@@ -1,9 +1,14 @@
 public class test {
     public static void f1() {
-        System.out.println("f1" + "#");
+        System.out.println(addSharp("f1"));
     }
 
     public static void main(String[] args) {
         f1();
+
+    }
+
+    public static String addSharp(String inp) {
+        return inp + "#";
     }
 }
