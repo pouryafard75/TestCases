063fa8cb2f142d23f2932ac7cba5641ac10a246a
Pouryafard75 <pouryafard75@gmail.com>
2023-01-04 06:24:25 -0500

    Anonymous class test

---
+++ case/v1/addSharpTest.java
@@ -0,0 +1,23 @@
+package addSharp;
+
+public class addSharpTest {
+   void test(){
+    return ConfirmationDialog.create(this, id,
+                    R.string.dialog_confirm_delete_title, "",
+                    R.string.dialog_confirm_delete_confirm_button,
+                    R.string.dialog_confirm_delete_cancel_button,
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            triggerDelete();
+                            finish();
+                        }
+                    },
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                        }
+                    });
+   }
+}
