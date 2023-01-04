61471e8013e11b0b7f3955f79db41305e955df6e
pouryafard <pouryafard75@gmail.com>
2023-01-04 14:13:43 -0500

    v1

--- bug/test.java
+++ bug/test.java
@@ -1,9 +1,11 @@
 public class v1 {
-
-    void main() {
-        Set<Integer> ourSegments = SPLIT_CONCAT_STRING_PATTERN;
-
-    }
+    private static EditScriptGenerator i1 = new EditScriptGenerator() {
+			@Override
+			public EditScript computeActions(ExtendedMultiMappingStore mappings, Map<String, TreeContext> parentContextMap, Map<String, TreeContext> childContextMap) {
+				sleep(20);
+				return null;
+			}
+		};
 }
 
 
