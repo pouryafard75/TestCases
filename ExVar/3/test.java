c083d5fc349e7f249b844efcdbf9b816c155d07d
Pouryafard75 <pouryafard75@gmail.com>
2023-01-23 04:54:06 -0500

    ExVar 3 v2

--- ExVar/3/test.java
+++ ExVar/3/test.java
@@ -1,20 +1,21 @@
-package exVar.2.v1;
+package exVar.2.v2;
 
 public class test {
     private Drafts getDraftsForCurrentState() {
         Drafts drafts = new Drafts();
-    
         if (!Util.isEmpty(composeText)) {
           drafts.add(new Draft(Draft.TEXT, composeText.getText().toString()));
         }
     
         for (Slide slide : attachmentManager.getSlideDeck().getSlides()) {
-          if      (slide.hasAudio()) drafts.add(new Draft(Draft.AUDIO, slide.getUri().toString()));
-          else if (slide.hasVideo()) drafts.add(new Draft(Draft.VIDEO, slide.getUri().toString()));
-          else if (slide.hasImage()) drafts.add(new Draft(Draft.IMAGE, slide.getUri().toString()));
-        }
+          String draftType = null;
+          if      (slide.hasAudio()) draftType = Draft.AUDIO;
+          else if (slide.hasVideo()) draftType = Draft.VIDEO;
+          else if (slide.hasImage()) draftType = slide.isEncrypted() ? Draft.ENCRYPTED_IMAGE : Draft.IMAGE;
     
+          if (draftType != null)
+            drafts.add(new Draft(draftType, slide.getUri().toString()));
+        }
         return drafts;
       }
-    
 }
