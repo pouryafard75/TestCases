73b0290cba87ebf3b6546f4f1281b2d48f4a63f8
Pouryafard75 <pouryafard75@gmail.com>
2023-01-23 03:24:06 -0500

    ExVar 1 v2

--- ExVar/1/Run.java
+++ ExVar/1/Run.java
@@ -1,10 +1,14 @@
-// Before refactoring:
+// After refactoring:
 package com.jfinal.core;
 public class Run {
     public static void main(String[] args) {
         int x = 5;
         int y = 10;
-        int result = (x * x) + (3 * x) + (y * y) + (5 * y);
+        int xSquared = x * x;
+        int threeX = 3 * x;
+        int ySquared = y * y;
+        int fiveY = 5 * y;
+        int result = xSquared + threeX + ySquared + fiveY;
         System.out.println(result);
     }
 }
