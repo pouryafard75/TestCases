b74627e558bd4fe0997e36049633e2074f97d4f0
Pouryafard75 <pouryafard75@gmail.com>
2023-03-13 21:24:23 -0400

    builder v1

--- temp/test.java
+++ temp/test.java
@@ -1,7 +1,13 @@
 public class test {
-    public void run()
-    {
-        ClassLoader loader = this.getClassLoader();
-        loader = 	Thread.currentThread().getContextClassLoader();
+    public void displayListFragment(Uri inputUri) {
+        ArrayList<Uri> uris = new ArrayList<>();
+        uris.add(inputUri);
+        DecryptFilesListFragment frag = DecryptFilesListFragment.newInstance(uris);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.decrypt_files_fragment_container, frag)
+                .addToBackStack("list")
+                .commit();
+
     }
 }
