1b2382eaeda59a873bd8c7aaadc972497ce5ca71
Pouryafard75 <pouryafard75@gmail.com>
2023-03-14 14:29:18 -0400

    temp builder v2

--- temp/test.java
+++ temp/test.java
@@ -1,5 +1,7 @@
 public class test {
     public void displayListFragment(ArrayList<Uri> inputUris) {
-        getSupportFragmentManager().beginTransaction().commit();
+        FragmentManager fragMan = getSupportFragmentManager();
+        FragmentTransaction trans = fragMan.beginTransaction();
+        trans.commit();
     }
 }
