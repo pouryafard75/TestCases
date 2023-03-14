9c4adfe79947a52a2bbcccf638bbf2a7e40e862b
Pouryafard75 <pouryafard75@gmail.com>
2023-03-14 14:27:50 -0400

    temp builder v1

--- temp/test.java
+++ temp/test.java
@@ -1,19 +1,5 @@
 public class test {
     public void displayListFragment(ArrayList<Uri> inputUris) {
-
-        DecryptFilesListFragment frag = DecryptFilesListFragment.newInstance(inputUris);
-
-        FragmentManager fragMan = getSupportFragmentManager();
-
-        FragmentTransaction trans = fragMan.beginTransaction();
-        trans.replace(R.id.decrypt_files_fragment_container, frag);
-
-        // if there already is a fragment, allow going back to that. otherwise, we're top level!
-        if (fragMan.getFragments() != null && !fragMan.getFragments().isEmpty()) {
-            trans.addToBackStack("list");
-        }
-
-        trans.commit();
-
+        getSupportFragmentManager().beginTransaction().commit();
     }
 }
