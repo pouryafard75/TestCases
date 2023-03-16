77896c9d96f5f66f219f3930459f1e98223f6ab2
Pouryafard75 <pouryafard75@gmail.com>
2023-03-16 02:00:09 -0400

    ambigous v1

--- temp/test.java
+++ temp/test.java
@@ -1,7 +1,25 @@
 public class test {
-    public void displayListFragment(ArrayList<Uri> inputUris) {
-        FragmentManager fragMan = getSupportFragmentManager();
-        FragmentTransaction trans = fragMan.beginTransaction();
-        trans.commit();
+    public static void main(String[] args) {
+        if(value == y) {
+            buffer.append( padWithZeros ? StringUtils.leftPad(""+years, count, "0") : ""+years ); 
+        } else
+        if(value == M) {
+            buffer.append( padWithZeros ? StringUtils.leftPad(""+months, count, "0") : ""+months ); 
+        } else
+        if(value == d) {
+            buffer.append( padWithZeros ? StringUtils.leftPad(""+days, count, "0") : ""+days ); 
+        } else
+        if(value == H) {
+            buffer.append( padWithZeros ? StringUtils.leftPad(""+hours, count, "0") : ""+hours ); 
+        } else
+        if(value == m) {
+            buffer.append( padWithZeros ? StringUtils.leftPad(""+minutes, count, "0") : ""+minutes ); 
+        } else
+        if(value == s) {
+            buffer.append( padWithZeros ? StringUtils.leftPad(""+seconds, count, "0") : ""+seconds ); 
+        } else
+        if(value == S) {
+            buffer.append( padWithZeros ? StringUtils.leftPad(""+milliseconds, count, "0") : ""+milliseconds ); 
+        }
     }
 }
