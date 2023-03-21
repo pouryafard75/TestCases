59ac497b136725d83169b1cd23a0d683b1a9812e
Pouryafard75 <pouryafard75@gmail.com>
2023-03-21 19:26:15 -0400

    MethodInvocation v2

--- v1/simple.java
+++ v1/simple.java
@@ -1,5 +1,5 @@
 public class VcapApplicationListener {
 	public static void main(String[] args) {
-		System.out.println("createDate (Default TimeZone: " + parameterSet.defaultTimeZone.getDisplayName(false, TimeZone.SHORT, Locale.ROOT) + "):");
+		System.out.println("createDate (Default TimeZone: expected=" + parameterSet.defaultTimeZone.getDisplayName(false, TimeZone.SHORT, Locale.ROOT) + "|current="+TimeZone.getDefault().getDisplayName()+"):");
 	}
 }
