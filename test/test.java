262ce7868cac6f023ddd2f754f23b2a1922faa1f
Pouryafard75 <pouryafard75@gmail.com>
2023-03-30 22:52:42 -0400

    V1 javadoc

--- test/test.java
+++ test/test.java
@@ -1,10 +1,14 @@
-public class test {
-    public void run()
-    {
-        Artifact jar = new DefaultArtifact(
-        artifact.getGroupId(),
-        artifact.getArtifactId(),
-        "jar",
-        artifact.getVersion());
-    }
+public class VcapApplicationListener {
+	/**
+     * Finds a valid path for a file from a FileStatus object.
+     * @param fileStatus FileStatus object corresponding to a file,
+     * or a directory.
+     * @param fileSystem FileSystem in with the file should be found
+     * @return The first file found
+     * @throws IOException
+     */
+
+	 public static Path depthFirstSearchForFile(final FileStatus[] statusArray, final FileSystem fileSystem) throws IOException {
+ 		return depthFirstSearchForFile(statusArray, fileSystem, null);
+	}
 }
