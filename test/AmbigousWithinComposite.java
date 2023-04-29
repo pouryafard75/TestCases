9feac8e5849a4f5e58ab366325f480492d2ad53d
Pouryafard75 <pouryafard75@gmail.com>
2023-04-29 02:34:48 -0400

    AmbigousWithinComposite v1

---
+++ test/AmbigousWithinComposite.java
@@ -0,0 +1,8 @@
+public class AmbigousWithinComposite {
+    public static void main(String[] args) {
+        for (result = sb.length(); result > 0; result --) {
+            System.out.println("Hello World");
+        }
+    }
+    
+}
