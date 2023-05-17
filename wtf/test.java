e41c5529e3fe28c803a19ad659f8875731169b6c
Pouryafard75 <pouryafard75@gmail.com>
2023-05-16 21:29:45 -0400

    v2 wtf

--- wtf/test.java
+++ wtf/test.java
@@ -2,16 +2,16 @@ package com.test;
 
 public class TestClass extends SuperClass {
 
-  public TestClass()
-  {
-    int var = 123;
-    int toBeDeleted = 5667;
-  }
+  public String newVar = "so new";
 
   private void lol()
   {
-    System.out.println("nix");
+
   }
 
-  private void newMethod()  { return; }
+  public TestClass()
+  {
+    int varUpd = 4444321;
+    System.out.println("nix");
+  }
 }
