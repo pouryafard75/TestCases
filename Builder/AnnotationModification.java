76d2b574cbbcc1af1d150a7375620ccfe0b661fc
Pouryafard75 <pouryafard75@gmail.com>
2023-06-07 23:22:24 -0400

    v2 annotation modification

--- Builder/AnnotationModification.java
+++ Builder/AnnotationModification.java
@@ -7,14 +7,16 @@ import static org.refactoringminer.astDiff.utils.UtilMethods.*;
 /**
  * @author  Pourya Alikhani Fard pouryafard75@gmail.com
  */
-@Disabled(value = "Check for class")
+@Disabled(value = "Check for class modification")
 public class AnnotationModification {
-    @Deprecated(since = "Tonight")
+    @SuppressWarnings(value = "testing")
+    @Deprecated(since = "Tonight but not tomorrow")
     private static final String dir = getDefects4jProblemsMappingPath();
 
-    @Deprecated
+    @Deprecated(since = "Tonight but not tomorrow")
     public static void main(String[] args) {
-        @SuppressWarnings(value = "testing")
+        @SuppressWarnings(value = "testing for fun")
+        @Deprecated(since = "Tonight but not tomorrow")
         int i = 1;
         System.out.println(i + dir);
     }
