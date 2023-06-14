26ce95e20f58f8132d2f25f29cef171a6f97fc7c
Pouryafard75 <pouryafard75@gmail.com>
2023-06-14 12:09:34 -0400

    v1 exp

---
+++ Builder/exp.java
@@ -0,0 +1,8 @@
+package Builder;
+
+public class exp {
+    public static void main(String[] args) {
+        flatten(properties, Collections.singletonMap("[" + (count++) + "]", object), key);
+    }
+    
+}
