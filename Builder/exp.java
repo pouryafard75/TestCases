cc388b755b40ffd061fcbf5845b305e4ccfb692a
Pouryafard75 <pouryafard75@gmail.com>
2023-06-15 16:12:21 -0400

    V2 exp statement

--- Builder/exp.java
+++ Builder/exp.java
@@ -2,7 +2,7 @@ package Builder;
 
 public class exp {
     public static void main(String[] args) {
-        double nOd = doubleValue();
+        long nOd = ((long) numerator) * object.denominator;
     }
     
 }
