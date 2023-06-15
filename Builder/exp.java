055882490eaafdc01d53ebf62ece2324ee0ac36c
Pouryafard75 <pouryafard75@gmail.com>
2023-06-15 16:12:12 -0400

    V1 exp statement

--- Builder/exp.java
+++ Builder/exp.java
@@ -2,7 +2,7 @@ package Builder;
 
 public class exp {
     public static void main(String[] args) {
-        flatten(properties, Collections.singletonMap(itemKey, item), key)
+        double nOd = doubleValue();
     }
     
 }
