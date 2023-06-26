083ec23bc39ad46ebec7c68cb3931ee41891522e
Pouryafard75 <pouryafard75@gmail.com>
2023-06-26 12:58:09 -0400

    Ambg Composite v2

--- Builder/v1.java
+++ Builder/v1.java
@@ -2,8 +2,7 @@ package Builder;
 
 public class v1 {
     public static void main(String[] args) {
-        for (result = offset; result < sb.length(); result ++) 
-        {
+        for (int result = offset; result < sb.length(); ++result) {
         }
     }
 }
