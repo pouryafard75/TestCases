f73e580d89fa63627601699a683d08f87d074c6a
Pouryafard75 <pouryafard75@gmail.com>
2023-07-01 03:08:25 -0400

    v2 builder

--- Builder/builderPattern.java
+++ Builder/builderPattern.java
@@ -3,6 +3,7 @@ public class builderPattern
     public static void main(String[] args) {
         KubernetesListBuilder builder = new KubernetesListBuilder()
                 .withLivenessProbe(getLivenessProbe())
+                .withReadinessProbe(getReadinessProbe())
                 .endContainer()
                 .withVolumes(getVolumes())
                 .endSpec()
