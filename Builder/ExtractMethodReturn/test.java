4a41f8440bc4d0b4e2f92832d1d9418708eeb9eb
Pouryafard75 <pouryafard75@gmail.com>
2023-07-11 17:58:33 -0400

    v1 exmethod return

---
+++ Builder/ExtractMethodReturn/test.java
@@ -0,0 +1,18 @@
+public class test{
+    private int a = 10;
+    private static class InterruptionTests {
+
+        private boolean isUnSubscribed;
+        private RuntimeException error;
+        private CountDownLatch latch = new CountDownLatch(1);
+
+        private Observable<Void> createObservable() {
+            return Observable.<Void>never().doOnUnsubscribe(new Action0() {
+                @Override
+                public void call() {
+                    isUnSubscribed = true;
+                }
+            });
+        }
+    }
+}
