9b7310575732bd01df94f6c448df1d1df2b457a9
Pouryafard75 <pouryafard75@gmail.com>
2023-07-17 13:23:42 -0400

    v1 jd

--- bug/v1.java
+++ bug/v1.java
@@ -1,12 +1,10 @@
 public class v1 {
 
+    /**
+	 * @see #219
+	 */
     void main() {
-        for (Filter filter : filters.getFilters()) {
-            if (filter instanceof ExternalResourceHolder) {
-                final Set<String> locations = ((ExternalResourceHolder) filter).getExternalResourceLocations();
-                externalResources.addAll(locations);
-            }
-        }
+        int a = 2;
     }
 }
 
