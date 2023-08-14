2c32d7c637512e6f352b2e1ae87068da83e8f511
Pouryafard75 <pouryafard75@gmail.com>
2023-08-14 14:11:46 -0400

    V1 test

--- case/test.java
+++ case/test.java
@@ -1,9 +1,5 @@
 public class test {
-    BG bg =  new BG(){
-        @Override
-        public void run(){
-            sleep(10);
-            System.out.println();
-        }
-    };
+    public void run(){
+            String pwd=password.optimize(session).getValue(session).getString();
+    }
 }
