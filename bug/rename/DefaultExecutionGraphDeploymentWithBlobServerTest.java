35e5817764ca1000353ba2be77a4f96129cf3fea
Pouryafard75 <pouryafard75@gmail.com>
2024-01-07 17:36:22 -0500

    rename issue v2

--- bug/rename/DefaultExecutionGraphDeploymentWithBlobServerTest.java
+++ bug/rename/DefaultExecutionGraphDeploymentWithBlobServerTest.java
@@ -1,41 +1,38 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.rules.TemporaryFolder;
-import org.mockito.Matchers;
-import org.mockito.Mockito;
 
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doAnswer;
 
 /**
  * Tests {@link ExecutionGraph} deployment when offloading job and task information into the BLOB
  * server.
  */
-public class DefaultExecutionGraphDeploymentWithBlobServerTest
+class DefaultExecutionGraphDeploymentWithBlobServerTest
         extends DefaultExecutionGraphDeploymentTest {
 
-    @ClassRule public static final TemporaryFolder TEMPORARY_FOLDER = new TemporaryFolder();
+    @TempDir Path temporaryFolder;
 
     private Set<byte[]> seenHashes =
             Collections.newSetFromMap(new ConcurrentHashMap<byte[], Boolean>());
 
     protected BlobServer blobServer = null;
 
-    @Before
+    @BeforeEach
     public void setupBlobServer() throws IOException {
         Configuration config = new Configuration();
         // always offload the serialized job and task information
         config.setInteger(BlobServerOptions.OFFLOAD_MINSIZE, 0);
         blobServer =
-                Mockito.spy(
-                        new BlobServer(config, TEMPORARY_FOLDER.newFolder(), new VoidBlobStore()));
+                new AssertBlobServer(
+                        config, TempDirUtils.newFolder(temporaryFolder), new VoidBlobStore());
         blobWriter = blobServer;
         blobCache = blobServer;
 
