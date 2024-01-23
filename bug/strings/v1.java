d5016efb58d7c991127153ff0fa5b6f7bf5b2b43
Pouryafard75 <pouryafard75@gmail.com>
2024-01-23 11:02:55 -0500

    Revert "strings bug v2"
    This reverts commit 7ccfafb7f7e4430362b22c3aa5af27cf84ac3d9a.

--- bug/strings/v1.java
+++ bug/strings/v1.java
@@ -1,6 +1,5 @@
 class v1{
     run() {
-        throw new StoreLockException( "Unable to obtain lock on store lock file: " + storeLockFile+". Please ensure no other process is using this database, and that the directory is writable (required even for read-only access)", e );
-
+        return new StoreLockException( message + ". " + help, e );
     }
 }
