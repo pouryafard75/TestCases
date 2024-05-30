a70839e80d5086a5be0862c703aec39d6df9bf07
Pouryafard75 <pouryafard75@gmail.com>
2024-05-30 11:13:11 -0400

    v1 jd

---
+++ bug/jd.java
@@ -0,0 +1,25 @@
+public class jd{
+
+
+    // /**
+    //  * Tells if the response wrapper has called <code>saveContext()</code> because of this
+    //  * wrapper.
+    //  */
+    // void a(){
+
+    // }
+
+    // /**
+    //  * Tells if the response wrapper has called <code>saveContext()</code> because of this wrapper.
+    //  */
+    // void b(){
+
+    // }
+
+    /** 
+     * Start a thread that will perform autobalancing in the background if appropriate to do so 
+    */
+    void d(){
+
+    }
+}
