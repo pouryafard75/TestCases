80c58c1876f8813edceed0f58ebbbe362479f776
Pouryafard75 <pouryafard75@gmail.com>
2024-05-30 13:55:04 -0400

    v1 javad

---
+++ bug/javad/X.java
@@ -0,0 +1,13 @@
+class X
+{
+    /**
+     * For a particular task with a copy on a failed host, attempt to find a suitable replica; mark it degraded otherwise
+     *
+     * @param task           The task to modify
+     * @param hosts          A list of available hosts
+     * @param failedHostUuid The host being failed
+     */
+    private void attemptFixTaskForFailedHost(JobTask task, List<HostState> hosts, String failedHostUuid) {
+        sleep("oops");
+    }
+}
