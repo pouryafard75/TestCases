aa06ddc867b5d5ba4038ac5b1e107b6435eea21e
Pouryafard75 <pouryafard75@gmail.com>
2024-06-02 17:41:17 -0400

    v1 jd

--- bug/javad/X.java
+++ bug/javad/X.java
@@ -1,14 +1,7 @@
 class X
 {
     /**
-     * For a particular task with a copy on a failed host, attempt to find a suitable replica; mark it degraded
-     * otherwise
-     *
-     * @param task           The task to modify
-     * @param hosts          A list of available hosts
-     * @param failedHostUuid The host being failed
-     */
-    private void attemptFixTaskForFailedHost(JobTask task, List<HostState> hosts, String failedHostUuid) {
-        sleep("oops");
-    }
+    * An allocator that allocates a block in the storage dir with most free space.
+    */
+    void A() {}
 }
