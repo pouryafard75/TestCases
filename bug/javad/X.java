e6eca32df8a6da7fa93697ab1cf2422a4d3a3621
Pouryafard75 <pouryafard75@gmail.com>
2024-06-02 17:41:42 -0400

    v2 jd

--- bug/javad/X.java
+++ bug/javad/X.java
@@ -1,7 +1,8 @@
 class X
 {
     /**
-    * An allocator that allocates a block in the storage dir with most free space.
+    * An allocator that allocates a block in the storage dir with most free space. It always allocates
+    * to the highest tier if the requested block store location is any tier.
     */
     void A() {}
 }
