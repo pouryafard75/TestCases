4cc9d9b216fe812c43506122c6535028526961f2
Pouryafard75 <pouryafard75@gmail.com>
2024-12-05 14:45:14 -0500

    simple code

--- bug/ifcond/v.java
+++ bug/ifcond/v.java
@@ -1,8 +1,5 @@
-public class v {
-    public void x(){
-        if (!((f1 instanceof BstVMVisitor.Identifier) || (f1 instanceof ParseTree))
-                && ((f2 instanceof BstVMVisitor.Identifier) || (f2 instanceof ParseTree))) {
-            throw new BstVMException("Expecting two functions for while$ (line %d)".formatted(ctx.start.getLine()));
-        }
+class C {
+    void m(){
+        sleep(5);
     }
 }
