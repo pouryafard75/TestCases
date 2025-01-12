2ae5237e9826e8480abdd9f3d1925b6d34e4efd0
Pouryafard75 <pouryafard75@gmail.com>
2025-01-11 19:59:35 -0500

    v1

---
+++ reorderWithModification/test.java
@@ -0,0 +1,9 @@
+class test{
+    void X()
+    {
+        String.format("Cannot register method '%s' as an @%s method since it is not static.",
+				method.getName(), annotationType.getSimpleName());
+    }
+
+
+}
