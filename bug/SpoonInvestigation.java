f5380855caba22ce65ba4564f0183a2b94885dba
Pouryafard75 <pouryafard75@gmail.com>
2025-03-23 20:13:26 -0400

    V1

--- bug/SpoonInvestigation.java
+++ bug/SpoonInvestigation.java
@@ -2,27 +2,38 @@ public class SpoonInvestigation {
     private void x(){
         sleep(1);
     }
-    public void testRedirectsAreProxied() throws Exception
+    @Test
+    public void testHeadersListedByConnectionHeaderAreRemoved() throws Exception
     {
+        final Map<String, String> hopHeaders = new LinkedHashMap<>();
+        hopHeaders.put(HttpHeader.TE.asString(), "gzip");
+        hopHeaders.put(HttpHeader.CONNECTION.asString(), "Keep-Alive, Foo, Bar");
+        hopHeaders.put("Foo", "abc");
+        hopHeaders.put("Foo", "def");
+        hopHeaders.put(HttpHeader.KEEP_ALIVE.asString(), "timeout=30");
         startServer(new HttpServlet()
         {
             @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+            protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
             {
-                if (req.getHeader("Via") != null)
-                    resp.addHeader(PROXIED_HEADER, "true");
-                resp.sendRedirect("/");
+                List<String> names = Collections.list(request.getHeaderNames());
+                for (String name : names)
+                {
+                    if (hopHeaders.containsKey(name))
+                        throw new IOException("Hop header must not be proxied: " + name);
+                }
             }
         });
         startProxy();
         startClient();
 
-        client.setFollowRedirects(false);
-
-        ContentResponse response = client.newRequest("localhost", serverConnector.getLocalPort())
+        Request request = client.newRequest("localhost", serverConnector.getLocalPort());
+        for (Map.Entry<String, String> entry : hopHeaders.entrySet())
+            request.header(entry.getKey(), entry.getValue());
+        ContentResponse response = request
                 .timeout(5, TimeUnit.SECONDS)
                 .send();
-        Assert.assertEquals(302, response.getStatus());
-        Assert.assertTrue(response.getHeaders().containsKey(PROXIED_HEADER));
+
+        Assert.assertEquals(200, response.getStatus());
     }
 }
