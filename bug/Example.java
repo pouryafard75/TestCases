383a2a200e1272a4171849e813efa3f7287b7024
Pouryafard75 <pouryafard75@gmail.com>
2025-03-25 17:58:56 -0400

    V2

--- bug/Example.java
+++ bug/Example.java
@@ -1,10 +1,11 @@
 /**
- * @author John
+ * @createdBy John
  * @version 1.0
  */
 public class Example {  
     void run() {
-        if (1 < 2) //code comment
-            Collections.<Integer> emptyList();
+        if (1 <= 2) //code comment (modified)
+            Collections.<String> emptyList();
     }
+    //newly added comment
 }
