65d0c729809b4abee2df8c241056767f0cdd8083
Pouryafard75 <pouryafard75@gmail.com>
2025-05-27 14:09:13 -0400

    v2 missing

--- bug/missingSubtrees/test.java
+++ bug/missingSubtrees/test.java
@@ -1,14 +1,14 @@
 package bug.missingSubtrees;
 
-public sealed class test permits A {
-    void X() throws RuntimeException{
+public sealed class test permits A2 {
+    void X() throws RuntimeException2{
         sleep(3);
     }
 }
-class B extends A {
+class B extends A2 {
     void m2();
 }
 
-class C implements I {
+class C implements I2 {
     void m1();
 }
