76bed70eddb84137e0fa1f7ac15235fae69d32f5
Pouryafard75 <pouryafard75@gmail.com>
2025-05-28 13:16:14 -0400

    lrhs e2 v2

--- bug/missingSubtrees/test.java
+++ bug/missingSubtrees/test.java
@@ -4,6 +4,8 @@ import java.util.TreeSet;
 
 public sealed class test permits A2 {
     void X(){
-        current[c] = createFileEntry(parent, files[c]);
+        actual[c] = createFileEntry(parent, current[c]);
+
+
     }
 }
