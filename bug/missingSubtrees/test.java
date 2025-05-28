da5f72106aaeb7ec33c5bad3260cc3fde812b9d0
Pouryafard75 <pouryafard75@gmail.com>
2025-05-28 12:52:14 -0400

    lrhs v2

--- bug/missingSubtrees/test.java
+++ bug/missingSubtrees/test.java
@@ -4,6 +4,6 @@ import java.util.TreeSet;
 
 public sealed class test permits A2 {
     void X(){
-        TreeSet<Keyword> treeSet = new TreeSet<Keyword>(tm.values());
+        TreeSet<Keyword> treeSet = new TreeSet<>(tm.values());
     }
 }
