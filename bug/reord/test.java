fb4498be010c788a40c108ce6592b29a205634da
Pouryafard75 <pouryafard75@gmail.com>
2025-08-09 01:08:41 -0400

    v2 reorder

--- bug/reord/test.java
+++ bug/reord/test.java
@@ -1,7 +1,7 @@
 public class test {
     void x(){
         fileContextMenu.getItems().add(actionFactory.createMenuItem(
-                StandardActions.OPEN_EXTERNAL_FILE, new OpenExternalFileAction(dialogService, stateManager, preferences, entry, file, taskExecutor)));
+                StandardActions.OPEN_EXTERNAL_FILE, new OpenSingleExternalFileAction(dialogService, preferences, entry, file, taskExecutor, stateManager)));
     }
     
 }
